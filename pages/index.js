c155f1ca726b492d02b6d53267d58d5f712b5f9e
berkaybilgis <berkaybilgis8@gmail.com>
2023-04-07 22:01:42 +0300

    footer component eklendi

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="todoapp">
-      <Header />
-    </div>
+    <>
+      <div className="todoapp">
+        <Header />
+      </div>
+      <Footer />
+    </>
   );
 }
